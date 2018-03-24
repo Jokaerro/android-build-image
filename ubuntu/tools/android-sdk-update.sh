54f2a740fc5b296a36bf428af93d7bdb50e76efd
Andrew Tesla <korvin7.64@gmail.com>
2018-03-24 14:38:44 +0500

    added other images

---
+++ ubuntu/tools/android-sdk-update.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+mkdir -p /opt/android-sdk-linux/bin/
+cp /opt/tools/android-env.sh /opt/android-sdk-linux/bin/
+source /opt/android-sdk-linux/bin/android-env.sh
+
+built_in_sdk=1
+
+
+echo $#
+
+echo $1
+
+built_in_sdk=1
+
+cd ${ANDROID_HOME}
+echo "Set ANDROID_HOME to ${ANDROID_HOME}"
+
+if [ -f sdk-tools-linux.zip ]
+then
+  echo "SDK Tools already bootstrapped. Skipping initial setup"
+else
+  echo "Bootstrapping SDK-Tools"
+  wget -q https://dl.google.com/android/repository/sdk-tools-linux-3859397.zip -O sdk-tools-linux.zip
+  unzip sdk-tools-linux.zip
+fi
+
+echo "Make sure repositories.cfg exists"
+mkdir -p ~/.android/
+touch ~/.android/repositories.cfg
+
+echo "Copying Licences"
+cp -rv /opt/licenses ${ANDROID_HOME}/licenses
+
+echo "Copying Tools"
+mkdir -p ${ANDROID_HOME}/bin
+cp -v /opt/tools/*.sh ${ANDROID_HOME}/bin
+
+echo "Installing packages"
+android-accept-licenses.sh "sdkmanager --package_file=/opt/tools/package-list.txt --verbose"
+
+echo "Updating SDK"
+update_sdk
+
+echo "Accepting Licenses"
+android-accept-licenses.sh "sdkmanager --licenses --verbose"
